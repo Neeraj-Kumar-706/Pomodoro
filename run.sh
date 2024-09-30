aa262050b10cc0009b2989d40be507c5e5319ba7
Neeraj-Kumar-706 <neerajkumar981879@gmail.com>
2024-09-30 21:08:23 +0530

    using pyenv runtime

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Run the Pomodoro app using this script from your terminal or command prompt if add it ob to your PATH
 # you can change this dir to wherever you want
-cd /mnt/88c49251-2b3d-489c-be49-24b3d296dd4b/project_decdo/Pomodomo
+cd /mnt/88c49251-2b3d-489c-be49-24b3d296dd4b/project_decdo/Pomodoro
 # set path for your environment
-source env/bin/activate
+source .venv/bin/activate
 # Run the Pomodoro app
-python app.py & disown # use app.py for or tapp.py optimized and better version
+python app.py & disown # use app.py for better version
 # deactivate environment
 deactivate
-exit 
 # Close the terminal
+exit 
+
 
